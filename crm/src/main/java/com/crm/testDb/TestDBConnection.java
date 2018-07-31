b13bc7c95364753379cd90b40b2454c183624f2b
Ferdis17 <fmuriyesu@users.noreply.github.com>
2018-07-31 12:58:59 -0400

    pom
    completed

---
+++ crm/src/main/java/com/crm/testDb/TestDBConnection.java
@@ -0,0 +1,36 @@
+package com.crm.testDb;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestDBConnection {
+
+	public static void main(String[] args) {
+		
+		String user = "admin1";
+		String pass = "student";
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/customer_management?useSSL=false&serverTimezone=UTC";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		
+		// get connection to database
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			
+			Class.forName(driver);
+			
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("SUCCESS!!!");
+			
+			myConn.close();
+			
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+
+	}
+
+}
